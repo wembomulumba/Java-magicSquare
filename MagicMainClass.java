fda1bd8795ef39eb277e2da06055e61eb9a82cdc
Wembo Mulumba <fabriceotepa@hotmail.com>
2018-06-18 05:02:43 -0400

    Update MagicMainClass.java

--- MagicMainClass.java
+++ MagicMainClass.java
@@ -1,5 +1,10 @@
 package Trees;
+/*
+  Author : Wembo otepa Mulumba
+  
+  Main Class calling the class MagicMainClass
 
+ */
 public class MagicMainClass {
     public static void main(String[] args){
         MagicSquare data = new MagicSquare(3);
